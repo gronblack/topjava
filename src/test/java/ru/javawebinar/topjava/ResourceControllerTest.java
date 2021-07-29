d6add414f0059f470a47098564375a107075c258
gronblack <30817266+gronblack@users.noreply.github.com>
2021-07-29 13:41:37 +0700

    Controllers tests

---
+++ src/test/java/ru/javawebinar/topjava/ResourceControllerTest.java
@@ -0,0 +1,22 @@
+package ru.javawebinar.topjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import ru.javawebinar.topjava.web.AbstractControllerTest;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class ResourceControllerTest extends AbstractControllerTest {
+    private static final MediaType TEXT_CSS = new MediaType("text", "css");
+
+    @Test
+    void getStyle() throws Exception {
+        perform(get("/resources/css/style.css"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(TEXT_CSS));
+    }
+}
